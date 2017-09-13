4a7411ebaea31b1f0e865aa619222082f31f9ade
Aleks Rutins <alex@thehat.hattydomain>
2017-09-13 16:38:54 -0400

    Initial commit (hope it works!)

---
+++ main.js
@@ -0,0 +1,9 @@
+var Digit = {
+    extract(n, ...placeValues) {
+        var res = 0;
+        for(let place of placeValues) {
+            res += (Math.floor(n / place) - Math.floor(n / (place*10)))
+        }
+        return place;
+    }
+}
