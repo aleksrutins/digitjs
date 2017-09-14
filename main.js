4ab3e137b6ec875910595f92b30ce0d4a6704e12
Aleks Rutins <alex@thehat.hattydomain>
2017-09-14 18:25:33 -0400

    Yet another commit...

--- main.js
+++ main.js
@@ -5,5 +5,8 @@ var Digit = {
             res += (Math.floor(n / place) - (Math.floor(n / (place * 10))*10))*place
         }
         return res;
+    },
+    [Symbol.toStringTag]() {
+        return "This is a library of functions to help with digits.";
     }
 }
